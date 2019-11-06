aa32d2acfb23fac2df53d97c27690f7ef2e9d166
nivi <nivetharavirns@gmail.com>
2019-11-06 15:06:02 +0530

    git done

---
+++ firstgit.c
@@ -0,0 +1,21 @@
+lude<stdio.h>
+void main()
+{
+int num;
+printf("enter the random number \n");
+scanf("%d", &num);
+					
+
+if(num % 2 == 0)
+{
+
+printf("the entered number is even  \n" );
+
+		}
+		else
+					{	
+
+										printf("the number is odd \n");
+
+											}
+}
